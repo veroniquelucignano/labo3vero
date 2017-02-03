3c5a06009ad2ceac49c4183aa18b5f9f5f0b0328
veroniquelucignano <lucignano.veronique@courrier.uqam.ca>
2017-02-02 20:58:56 -0500

    #init

---
+++ src/lab3git/Lab3Git.java
@@ -0,0 +1,18 @@
+
+package lab3git;
+
+import java.time.LocalDate;
+
+
+
+public class Lab3Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Buongiorno a voi!");
+      
+    }
+    
+}
